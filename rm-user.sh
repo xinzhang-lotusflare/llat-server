5d67caf6b3784a8a9477785bbcc61e724728e748
xinzhang-lotusflare <79883589+xinzhang-lotusflare@users.noreply.github.com>
2022-11-02 15:52:18 +0800

    Execute removing with WireGuard script (#12)

--- rm-user.sh
+++ rm-user.sh
@@ -6,7 +6,7 @@ source ${UTIL_SH}
 USER_EMAIL_ADDR=$1
 
 rm() {
-  echo $(log_date) - Removing WireGuard credential for user: $USER_EMAIL_ADDR
+  echo $(log_date) - Remove WireGuard credential for user: $USER_EMAIL_ADDR
   USER_NAME=$(${PYTHON} ${GET_EMAIL_USERNAME_PY} ${USER_EMAIL_ADDR})
 
   mkdir -p ./${ACCOUNTS_STORAGE}/inactive
@@ -14,6 +14,11 @@ rm() {
   if [[ -e ./${ACCOUNTS_STORAGE}/active/${USER_NAME}.csv ]]; then
     mv ./${ACCOUNTS_STORAGE}/active/${USER_NAME}.csv ./${ACCOUNTS_STORAGE}/inactive/${USER_NAME}_$(today).csv
   fi
+
+  # Remove WireGuard config
+  echo $(log_date) - Removing
+  CLIENT_NAME=$(${PYTHON} ${GET_CLIENT_NAME_PY} ${USER_EMAIL_ADDR})
+  sudo ${PYTHON} ${WG_PY} rm -c ${CLIENT_NAME} -s ${WG_SH}
 }
 
 rm
